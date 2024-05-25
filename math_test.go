aa38008a0a494819be00c6c47c9cd33b9d638ea2
Fernando Rosito <fernando.rosito@gmail.com>
2024-05-25 17:31:34 -0300

    ci: Add github actions for every push

--- math_test.go
+++ math_test.go
@@ -5,8 +5,6 @@ func TesteSoma(t *testing.T) {
 	total := Soma(15, 15)
 
 	if total != 30 {
-		t.Errorf(
-			"Resultado da soma é invalido: Resultado %d. Esperado: %d", total, 30
-		)
+		t.Errorf("Resultado da soma é invalido: Resultado %d. Esperado: %d", total, 30)
 	}
 }
